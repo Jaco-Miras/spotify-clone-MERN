53806c6653c92d86e8a91726da2d6c0496cae5ee
Jaco-Miras <miras.jaco@gmail.com>
2025-01-12 12:30:15 +0800

    creating main layout and left sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/home/home.page";
 import { AuthCallbackPage } from "./pages/auth-callback/auth.callback.page";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import { MainLayout } from "./layout/main-layout";
+import { ChatPage } from "./pages/chat/chat-page";
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
         <Route
           path="/sso-callback"
           element={
@@ -16,6 +17,10 @@ const App = () => {
             />
           }
         />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/chat" element={<ChatPage />} />
+        </Route>
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
       </Routes>
     </>
