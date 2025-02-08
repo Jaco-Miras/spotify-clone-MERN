c7be7544c9d7bf71ecadda6cd98730bd08aca3d9
Jaco-Miras <miras.jaco@gmail.com>
2025-02-08 15:30:37 +0800

    creating home page

--- frontend/src/pages/home/home.page.tsx
+++ frontend/src/pages/home/home.page.tsx
@@ -1,9 +1,72 @@
 import { Topbar } from "@/components/topbar";
+import { useMusicStore } from "@/stores/useMusicStore";
+import { useEffect, useState } from "react";
+import { FeaturedSection } from "./featured-section";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { SectionGrid } from "./section-grid";
 
 export const HomePage = () => {
+  const [greeting, setGreeting] = useState("");
+
+  const {
+    fetchFeaturedSongs,
+    fetchMadeForYouSongs,
+    fetchTrendingSongs,
+    isLoading,
+    madeForYouSongs,
+    featuredSongs,
+    trendingSongs,
+  } = useMusicStore();
+
+  useEffect(() => {
+    fetchFeaturedSongs();
+    fetchMadeForYouSongs();
+    fetchTrendingSongs();
+  }, [fetchFeaturedSongs, fetchMadeForYouSongs, fetchTrendingSongs]);
+
+  useEffect(() => {
+    const updateGreeting = () => {
+      const hour = new Date().getHours();
+      if (hour < 12) {
+        setGreeting("Morning ☀️");
+      } else if (hour < 18) {
+        setGreeting("Afternoon 🌤");
+      } else {
+        setGreeting("Evening 🌙");
+      }
+    };
+
+    updateGreeting();
+    const interval = setInterval(updateGreeting, 60000);
+    return () => clearInterval(interval);
+  }, []);
+
+  console.log(madeForYouSongs, featuredSongs, trendingSongs);
   return (
-    <div>
+    <div className="rounded-md overflow-hidden h-full bg-gradient-to-b from-zinc-800 to-zinc-900">
       <Topbar />
+      <ScrollArea className="h-[calc(100vh-180px)]">
+        <div className="p-4 sm:p-6">
+          <h1 className="text-2xl sm:text-3xl font-bold mb-6">
+            Good {greeting}
+          </h1>
+          <FeaturedSection />
+
+          <div className="space-y-8">
+            <SectionGrid
+              title="Made For You"
+              songs={madeForYouSongs}
+              isLoading={isLoading}
+            />
+
+            <SectionGrid
+              title="Trending"
+              songs={trendingSongs}
+              isLoading={isLoading}
+            />
+          </div>
+        </div>
+      </ScrollArea>
     </div>
   );
 };
